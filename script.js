2c62a48c969070aae396bce2ab32e839bb448d04
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-09-17 10:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent form from submitting and refreshing the page
+    event.preventDefault();
 
+    // Get the form element
+    const form = document.getElementById('form1');
+
+    // Retrieve values of first and last name from the form
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+
+    // Display the values in an alert box
+    alert(firstName + ' ' + lastName);
 }
